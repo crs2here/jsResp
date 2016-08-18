64f47c34e98a37d0d5618e1a69f1176238633f1f
Chris Reyes <crs2here@gmail.com>
2016-08-18 13:10:44 -0700

    added Filtering and Sorting a List of Strings
    notes from http://www.jstips.co/en/filtering-and-sorting-a-list-of-strings/

--- main.js
+++ main.js
@@ -870,8 +870,24 @@ Function.prototype.apply passing the array-like object as the value of this:
 
 
 
+var keywords = ['do', 'if', 'in', 'for', 'new', 'try', 'var', 'case', 'else', 'enum', 'null', 'this', 'true', 'void', 'with', 'break', 'catch', 'class', 'const', 'false', 'super', 'throw', 'while', 'delete', 'export', 'import', 'return', 'switch', 'typeof', 'default', 'extends', 'finally', 'continue', 'debugger', 'function', 'do', 'if', 'in', 'for', 'int', 'new', 'try', 'var', 'byte', 'case', 'char', 'else', 'enum', 'goto', 'long', 'null', 'this', 'true', 'void', 'with', 'break', 'catch', 'class', 'const', 'false', 'final', 'float', 'short', 'super', 'throw', 'while', 'delete', 'double', 'export', 'import', 'native', 'public', 'return', 'static', 'switch', 'throws', 'typeof', 'boolean', 'default', 'extends', 'finally', 'package', 'private', 'abstract', 'continue', 'debugger', 'function', 'volatile', 'interface', 'protected', 'transient', 'implements', 'instanceof', 'synchronized', 'do', 'if', 'in', 'for', 'let', 'new', 'try', 'var', 'case', 'else', 'enum', 'eval', 'null', 'this', 'true', 'void', 'with', 'break', 'catch', 'class', 'const', 'false', 'super', 'throw', 'while', 'yield', 'delete', 'export', 'import', 'public', 'return', 'static', 'switch', 'typeof', 'default', 'extends', 'finally', 'package', 'private', 'continue', 'debugger', 'function', 'arguments', 'interface', 'protected', 'implements', 'instanceof', 'do', 'if', 'in', 'for', 'let', 'new', 'try', 'var', 'case', 'else', 'enum', 'eval', 'null', 'this', 'true', 'void', 'with', 'await', 'break', 'catch', 'class', 'const', 'false', 'super', 'throw', 'while', 'yield', 'delete', 'export', 'import', 'public', 'return', 'static', 'switch', 'typeof', 'default', 'extends', 'finally', 'package', 'private', 'continue', 'debugger', 'function', 'arguments', 'interface', 'protected', 'implements', 'instanceof'];
 
+var filteredAndSortedKeywords = keywords
+  .filter(function (keyword, index) {
+      return keywords.lastIndexOf(keyword) === index;
+    })
+  .sort(function (a, b) {
+      return a < b ? -1 : 1;
+    });
+    
+    //The ES6 (ECMAScript 2015) version using arrow functions looks a little simpler:
+const filteredAndSortedKeywords = keywords
+  .filter((keyword, index) => keywords.lastIndexOf(keyword) === index)
+  .sort((a, b) => a < b ? -1 : 1);
 
+console.log(filteredAndSortedKeywords);
+
+// ['abstract', 'arguments', 'await', 'boolean', 'break', 'byte', 'case', 'catch', 'char', 'class', 'const', 'continue', 'debugger', 'default', 'delete'
 
 
 
